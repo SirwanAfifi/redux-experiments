8c525d74b69eec39c251e909dce8d368d27c3c7e
sirwan <sir1afifi@gmail.com>
2019-11-26 00:26:07 +0330

    Add devTools

--- blog/src/index.js
+++ blog/src/index.js
@@ -6,8 +6,11 @@ import { createStore } from "redux";
 import App from "./components/App";
 import reducers from "./reducers";
 
+const DEBUGGER_MIDDLEWARE = window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__();
+
 ReactDOM.render(
-    <Provider store={createStore(reducers)}>
+    <Provider store={createStore(reducers, DEBUGGER_MIDDLEWARE)}>
         <App />
     </Provider>,
     document.querySelector("#root")
