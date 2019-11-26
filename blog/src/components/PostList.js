e7ffd9b5fe1fb6522f1ef65f105cff875f7162c2
sirwan <sir1afifi@gmail.com>
2019-11-26 10:45:42 +0330

    dispatch action creator

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,11 +1,18 @@
 import React from 'react';
-
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions";
 class PostList extends React.Component {
-    render() {
-        return (
-            <div>Post List</div>
-        );
-    }
+	componentDidMount() {
+		this.props.fetchPosts();
+	}
+
+	render() {
+		return (
+			<div>Post List</div>
+		);
+	}
 }
 
-export default PostList;
+export default connect(null, {
+	fetchPosts
+})(PostList);
