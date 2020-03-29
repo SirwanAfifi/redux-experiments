42d570ec1ac0a134273265cdf0e0160a6322feb2
sirwan <sir1afifi@gmail.com>
2020-03-29 19:48:18 +0430

    Dynamic name

--- start/src/reducer.js
+++ start/src/reducer.js
@@ -1,30 +1,13 @@
 import { addTodo, removeTodo } from "./actions";
 import { createReducer } from "@reduxjs/toolkit";
 
-export default function reducer(state = { todos: [] }, action) {
-  switch (action.type) {
-    case addTodo.type:
-      return {
-        ...state,
-        todos: [...state.todos, action.payload]
-      };
-    case removeTodo.type:
-      return {
-        ...state,
-        todos: state.todos.filter(todo => todo.id !== action.payload)
-      };
-    default:
-      return state;
-  }
-}
-
-createReducer(
+export default createReducer(
   { todos: [] },
   {
-    addTodo: (state, action) => {
+    [addTodo.type]: (state, action) => {
       state.todos.push(action.payload);
     },
-    removeTodo: (state, action) => {
+    [removeTodo.type]: (state, action) => {
       const index = state.todos.findIndex(todo => todo.id === action.payload);
       state.todos.splice(index);
     }
